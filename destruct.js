5095c30f501943790256a0da26c22723a34912e0
AishaKeita <aishacassel@gmail.com>
2024-04-22 15:07:35 +0200

    Experimenting destructuring

---
+++ destruct.js
@@ -0,0 +1,11 @@
+const person = {name:  "John", age: 25 }
+const {name, age} = person
+
+console.log(person.name);
+console.log(person.age);
+
+const color = ["red", "green", "yellow"];
+const [firstColor, secondColor] = color;
+
+console.log(firstColor);
+console.log(secondColor);
