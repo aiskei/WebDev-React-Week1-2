cc5aabf3e7fd41434b2a29eedefb8550e9e969fe
AishaKeita <aishacassel@gmail.com>
2024-04-23 11:37:56 +0200

    Javascript tanary operator

---
+++ modules/tenaryOperator.js
@@ -0,0 +1,6 @@
+// Tenary operators helps in writing conditional statement base on if the condition is true or false
+
+const age = 20;
+const message = age >= 18 ? 'You are an adult' : 'You are a minor'
+
+console.log(message);
